6d4da5f45d35d02d928c523006d21208a2aa2c0d
Edwin Silva <ea.silval1@uniandes.edu.co>
2025-04-03 23:58:59 -0500

    Crear funcionalidades para BFF Mobile de login y obtener info de usuario por token.

--- bff/mobile-bff/src/blueprints/users_blueprint.py
+++ bff/mobile-bff/src/blueprints/users_blueprint.py
@@ -23,3 +23,27 @@ def create_user():
     logging.debug("Sending information to create new user from BFF Web.")
     adapter = UsersAdapter()
     return adapter.create_user(user_data)
+
+@users_blueprint.route('/auth', methods=['POST'])
+def authorize_user():
+    logging.debug("Received request to authorize user.")
+    auth_data = request.get_json()
+    if not auth_data or not all(key in auth_data for key in ('email', 'password')):
+        logging.error("Missing required fields in request auth_data.")
+        return jsonify({'msg': 'Faltan campos requeridos.'}), 400
+
+    logging.debug("Sending information to authorize user from BFF Web.")
+    adapter = UsersAdapter()
+    return adapter.authorize(auth_data['email'], auth_data['password'])
+
+@users_blueprint.route('/me', methods=['GET'])
+def get_user_info():
+    logging.debug("Received request to get user info.")
+    token = request.headers.get('Authorization')
+    if not token:
+        logging.error("Missing Authorization header.")
+        return jsonify({'msg': 'Unauthorized'}), 401
+
+    logging.debug("Sending information to get user info from BFF Web.")
+    adapter = UsersAdapter()
+    return adapter.get_user_info(token)

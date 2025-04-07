14a52bdb2f33f53b8fc37c936974003e3e33beed
SystemsArchitect <email@no-reply.com>
2025-04-06 22:05:16 -0500

    @MaAvilaT definiendo los pipelines de ejecucion de test cases y el pipeline de CI para con nuestros desplieges + estandarizando los versionamientos de las images de software en la rama main

--- deployment/build_images.sh
+++ deployment/build_images.sh
@@ -12,34 +12,50 @@ then
   source ./secrets/secrets.env.sh
 fi
 
+
 cd ../api || exit 1
 ROOT_DIR="$(pwd)"
 
 export APIS
 
+
+
+TIMESTAMP="$(date +%Y%m%d%H%M%S)"
+
 if [[ "$1" == "--apis" || "$1" == "--push-all" ]]
 then
+  echo "========================================================================"
   echo "[INFO]: Building docker images for APIs"
+  echo "========================================================================"
 
   for folder in "${!APIS[@]}"; do
     cd "$ROOT_DIR" || exit 1
 
     if [[ -f "./$folder/Dockerfile" ]]
     then
+      UUID="$(find "./$folder/" -type f -exec sha256sum {} + | sort | sha256sum | cut -c1-6)"
       cd "./$folder/" || exit 1
+      VERSION="$(xargs < "version.txt")"
 
+      echo "========================================================================"
       echo "[INFO]: $(pwd) :: building image for ${APIS[$folder]}..."
-      docker build -t "$DOCKER_PATH${APIS[$folder]}:latest" -f "Dockerfile" .
+      echo "========================================================================"
+      docker build -t "$DOCKER_PATH${APIS[$folder]}:latest" "$DOCKER_PATH${APIS[$folder]}:$VERSION-$UUID-$TIMESTAMP" -f "Dockerfile" .
 
       if [[ "$1" == "push" || "$1" == "--push-all" ]]
       then
         docker push "$DOCKER_PATH${APIS[$folder]}:latest" || exit 1
       fi
     else
+      echo "========================================================================"
       echo "[INFO]: skipping ${APIS[$folder]} @ ./api/$folder  (no Dockerfile found)."
+      echo "========================================================================"
   fi
   done
 fi
+
+
+
 
 if [[ "$1" == "--bffs" || "$1" == "--push-all" ]]
 then
@@ -47,26 +63,36 @@ then
   cd ../bff || exit 1
   ROOT_DIR="$(pwd)"
 
+  echo "========================================================================"
   echo "[INFO]: Building images for BFF"
+  echo "========================================================================"
 
   for folder in "${!BACKENDS_FOR_FRONTEDS[@]}"; do
     cd "$ROOT_DIR" || exit 1
 
     if [[ -f "./$folder/Dockerfile" ]]
     then
+      UUID="$(find "./$folder/" -type f -exec sha256sum {} + | sort | sha256sum | cut -c1-6)"
       cd "./$folder/" || exit 1
 
+      VERSION="$(xargs < "version.txt")"
+
+      echo "========================================================================"
       echo "[INFO]: $(pwd) :: building image for ${BACKENDS_FOR_FRONTEDS[$folder]}..."
-      docker build -t "$DOCKER_PATH${BACKENDS_FOR_FRONTEDS[$folder]}:latest" -f "Dockerfile" .
+      echo "========================================================================"
+      docker build -t "$DOCKER_PATH${BACKENDS_FOR_FRONTEDS[$folder]}:latest" "$DOCKER_PATH${APIS[$folder]}:$VERSION-$UUID-$TIMESTAMP" -f "Dockerfile" .
 
       if [[ "$1" == "push" || "$1" == "--push-all" ]]
       then
         docker push "$DOCKER_PATH${BACKENDS_FOR_FRONTEDS[$folder]}:latest" || exit 1
       fi
     else
+      echo "========================================================================"
       echo "[INFO]: skipping ${BACKENDS_FOR_FRONTEDS[$folder]} @ ./bff/$folder  (no Dockerfile found)."
+      echo "========================================================================"
   fi
   done
 fi
+
 
 echo "[INFO]: ✅ Docker images built successfully."
